1542f6f8a08aad21916962ed76645af584fc1069
kyoztmy <85312054+kyoztmy@users.noreply.github.com>
2021-06-04 03:48:22 +0900

    upload 01

---
+++ zutomayo.js
@@ -0,0 +1,84 @@
+const addDiv = (parentDiv, className, callBack = null ) => {
+	const t = document.createElement("div");
+	t.classList.add(className);
+	if( callBack && typeof callBack === "function") callBack(t);
+	parentDiv.appendChild(t);
+	return t;
+}
+
+const createFace = () =>{
+	const clock = document.getElementById("clock");
+	let vp = [clock.clientWidth,clock.clientHeight];
+	let chokei = Math.min(...vp);
+	
+	const clockFace = addDiv(clock,"clock-face",( t )=>{
+		[t.style.height,t.style.width] = [chokei+"px",chokei+"px"];
+		[t.style.top,t.style.left] = [(vp[1]-chokei) / 2 + "px",(vp[0]-chokei) / 2 + "px"];
+	});
+
+	const r60 = 360 / 60;
+	const originX = clockFace.clientWidth / 2;
+	for( let i = 0 ; i < 60 ; i ++){
+		const deg = i * r60;
+		addDiv( clockFace,
+				i % 5 ===0 ? "clock-line1" : "clock-line2",
+				( t ) => {
+					if( i > 0 ) {
+						t.style.transformOrigin = `${originX}px center`;
+						t.style.transform=`rotate(${deg}deg)`;
+					}
+				}
+		);
+	}
+	
+	const r12 = 360 / 12;
+	const hankei = originX;
+	const moziPos = hankei -30 ;
+	const MathPi = Math.PI / 180;
+	for( let i = 0 ; i < 12 ; i ++){
+		const deg = i * r12;
+		addDiv( clock ,"clock-text",
+			( t ) =>{
+			const mojiX = hankei + moziPos * Math.sin( deg * MathPi );
+			const mojiY = hankei - moziPos * Math.cos( deg * MathPi );
+			[t.style.top,t.style.left] = [mojiY + "px",mojiX + "px"];
+			t.innerText = i === 0 ? "12" : i.toString();
+		});
+	}
+	
+	addDiv( clockFace , "clock-center" );
+};
+
+function timeRefresh () {
+	updateClock();
+	setTimeout(timeRefresh, 60000);
+}
+
+function updateClock(){
+	var now = moment();
+
+	//秒
+	var second = now.seconds() * 6;
+
+	//分
+	var minute = now.minutes() * 6 + second / 60;
+
+	//時
+	var hour = ((now.hours() % 12) / 12) * 360 + minute / 12;
+
+	
+	$("#hour").css("transform", "rotate(" + hour + "deg)");
+	$("#minute").css("transform", "rotate(" + minute + "deg)");
+	
+	var animation = document.createElement("style");
+	animation.innerHTML = "@keyframes rotation-s {0% {transform: rotate(" + second + "deg)}100% {transform: rotate(" + (second + 360) + "deg)}}";
+	document.head.appendChild(animation);
+	
+	//$("#second").css("transform", "rotate(" + second + "deg)");
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+	 createFace();
+	 timeRefresh();
+});
+
