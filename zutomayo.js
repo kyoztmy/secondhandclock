470b39a4e78ec26f1cb4a25fc85dfc6e63f4b8c0
kyoztmy <85312054+kyoztmy@users.noreply.github.com>
2021-06-04 17:57:19 +0900

    Add files via upload

--- zutomayo.js
+++ zutomayo.js
@@ -63,12 +63,10 @@ function timeRefreshSecond() {
 function updateSecond(){
 	var now = moment();
 	var second = now.seconds() * 6 + 1;
-	
-	document.documentElement.style.setProperty("--byousin-start-deg", second + "deg");
-	document.documentElement.style.setProperty("--byousin-end-deg", (second + 360) + "deg");
-	
-	//var second = now.seconds() * 6;
-	//$("#second").css("transform", "rotate(" + second + "deg)");
+
+	var animation = document.createElement("style");
+	animation.innerHTML = "@keyframes rotation-s {0% {transform: rotate(" + second + "deg)}100% {transform: rotate(" + (second + 360) + "deg)}} @-webkit-keyframes rotation-s {0% {transform: rotate(" + second + "deg)}100% {transform: rotate(" + (second + 360) + "deg)}} @-moz-keyframes rotation-s {0% {transform: rotate(" + second + "deg)}100% {transform: rotate(" + (second + 360) + "deg)}}";
+	document.head.appendChild(animation);
 }
 
 function updateClock(){
@@ -92,7 +90,7 @@ function updateText(){
     $("#ntime").html(moment().format('HH:mm'));
 }
 
-window.addEventListener('load', () => {
+window.addEventListener("DOMContentLoaded", () => {
 	 createFace();
 	 timeRefresh();
 	 timeRefreshSecond();
